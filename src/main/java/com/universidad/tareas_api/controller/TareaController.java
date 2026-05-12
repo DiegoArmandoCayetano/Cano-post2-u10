b9e1537ef6955dcee1daa0ee3c9b01337433d962
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-05-12 15:34:32 -0500

    Arreglar acrhivo tareaController

--- src/main/java/com/universidad/tareas_api/controller/TareaController.java
+++ src/main/java/com/universidad/tareas_api/controller/TareaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -14,13 +15,15 @@ public class TareaController {
     private final List<Tarea> tareas = new ArrayList<>();
 
     @PostMapping
-    public Tarea crearTarea(@RequestBody Tarea tarea) {
+    public ResponseEntity<Tarea> crearTarea(@RequestBody Tarea tarea) {
 
         tarea.setId((long) (tareas.size() + 1));
 
         tareas.add(tarea);
 
-        return tarea;
+        return ResponseEntity
+                .status(HttpStatus.CREATED) // 👈 IMPORTANTE PARA NEWMAN
+                .body(tarea);
     }
 
     @GetMapping("/{id}")
@@ -30,8 +33,8 @@ public class TareaController {
                 .filter(t -> t.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() ->
-    new ResponseStatusException(HttpStatus.NOT_FOUND)
-);
+                        new ResponseStatusException(HttpStatus.NOT_FOUND)
+                );
     }
 
     @PatchMapping("/{id}/completar")
@@ -41,8 +44,8 @@ public class TareaController {
                 .filter(t -> t.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() ->
-    new ResponseStatusException(HttpStatus.NOT_FOUND)
-);
+                        new ResponseStatusException(HttpStatus.NOT_FOUND)
+                );
 
         tarea.setCompletada(true);
 
